64ec355c9deed85320271287560a650490abd28a
Kanatello7 <jetrukanatello@gmail.com>
2026-01-18 21:55:14 +0500

    added schemas for user

---
+++ src/auth/schemas.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    password: str = Field(max_length=50)
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    image_file: str | None
+    image_path: str
--- src/posts/schemas.py
+++ src/posts/schemas.py
@@ -1,15 +1,23 @@
+from datetime import datetime
+from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict, Field
+
+from src.auth.schemas import UserResponse
 
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
-    content: str 
-    author: str 
+    content: str
+
 
 class PostCreate(PostBase):
-    pass 
+    user_id: UUID
+
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    date_posted: str 
+    user_id: UUID
+    date_posted: datetime
+    author: UserResponse
