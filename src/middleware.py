0b4c83958a4782d806d22a279b899c5b89b7238e
Kanatello7 <jetrukanatello@gmail.com>
2026-03-10 21:56:09 +0500

    ProcessTimeMiddleware refactoring

--- src/middleware.py
+++ src/middleware.py
@@ -4,6 +4,9 @@ import uuid
 from contextvars import ContextVar
 
 from fastapi import FastAPI
+from starlette.datastructures import MutableHeaders
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
+from typing_extensions import Literal
 
 request_id_ctx: ContextVar[str | None] = ContextVar("request_id", default=None)
 
@@ -74,27 +77,41 @@ class LoggingMiddleware:
 
 
 class ProcessTimeMiddleware:
-    def __init__(self, app):
-        self.app = app
+    app: ASGIApp
+    time_type_to_multiplier = {
+        "ms": 1000.0,
+        "s": 1.0,
+        "us": 1_000_000.0,
+        "ns": 1_000_000_000.0,
+    }
 
-    async def __call__(self, scope, receive, send):
+    def __init__(
+        self,
+        app: ASGIApp,
+        time_type: Literal["ms", "s", "us", "ns"],
+        header_key: str = "X-Process-Time",
+    ) -> None:
+        self.app = app
+        self._multiplier = self.time_type_to_multiplier[time_type]
+        self._header_key = header_key
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] != "http":
             await self.app(scope, receive, send)
             return
 
-        start_time = time.time()
+        start_time = time.perf_counter()
 
-        async def send_wrapper(message):
+        async def send_wrapper(message: Message) -> None:
             if message["type"] == "http.response.start":
-                duration = time.time() - start_time
-                headers = list(message.get("headers", []))
-                headers.append((b"x-process-time", f"{duration:.6f}".encode()))
-                message["headers"] = headers
+                headers = MutableHeaders(scope=message)
+                process_time = (time.perf_counter() - start_time) * self._multiplier
+                headers.append(self._header_key, str(process_time))
             await send(message)
 
         await self.app(scope, receive, send_wrapper)
 
 
 def add_middlewares_to_app(app: FastAPI):
-    app.add_middleware(ProcessTimeMiddleware)
+    app.add_middleware(ProcessTimeMiddleware, time_type="ms")
     app.add_middleware(LoggingMiddleware)
